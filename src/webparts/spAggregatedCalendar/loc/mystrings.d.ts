307a3470e48910bb418c79292c069dd388a1fbf8
kiyaka-motende <hkingjr.hm@gmail.com>
2023-02-13 14:34:57 +0300

    Initial commit

---
+++ src/webparts/spAggregatedCalendar/loc/mystrings.d.ts
@@ -0,0 +1,18 @@
+declare interface ISpAggregatedCalendarWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+  AppLocalEnvironmentSharePoint: string;
+  AppLocalEnvironmentTeams: string;
+  AppLocalEnvironmentOffice: string;
+  AppLocalEnvironmentOutlook: string;
+  AppSharePointEnvironment: string;
+  AppTeamsTabEnvironment: string;
+  AppOfficeEnvironment: string;
+  AppOutlookEnvironment: string;
+}
+
+declare module 'SpAggregatedCalendarWebPartStrings' {
+  const strings: ISpAggregatedCalendarWebPartStrings;
+  export = strings;
+}
